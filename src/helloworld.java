6b9832c75fecd9dd7ff1d1808946c4f5fcb73cb8
student <student@student-VirtualBox>
2013-09-28 17:50:52 +0200

    dodano wykrzykniki

--- src/helloworld.java
+++ src/helloworld.java
@@ -6,8 +6,8 @@ public class helloworld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		System.out.println("hello world !!");
 
 	}
-
+ 
 }
